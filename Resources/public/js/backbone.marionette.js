05318e10cc1a760f06bab3c7cb0980c4d2a1439c
Jeffrey Brubaker <jeffreymb@gmail.com>
2013-02-25 10:09:38 -0500

    update backbone.marionette to load modules from current directory.

--- Resources/public/js/backbone.marionette.js
+++ Resources/public/js/backbone.marionette.js
@@ -15,15 +15,15 @@
 (function (root, factory) {
   if (typeof exports === 'object') {
 
-    var jquery = require('jquery');
-    var underscore = require('underscore');
-    var backbone = require('backbone');
+    var jquery = require('./jquery');
+    var underscore = require('./underscore');
+    var backbone = require('./backbone');
 
     module.exports = factory(jquery, underscore, backbone);
 
   } else if (typeof define === 'function' && define.amd) {
 
-    define(['jquery', 'underscore', 'backbone'], factory);
+    define(['./jquery', './underscore', './backbone'], factory);
 
   } 
 }(this, function ($, _, Backbone) {
